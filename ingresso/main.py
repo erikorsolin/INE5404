ade159edfd807ae02aead3b005b8d8651b147d31
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-17 10:10:50 -0300

    testes no main

---
+++ ingresso/main.py
@@ -0,0 +1,18 @@
+from normal import Normal
+from camarote import CamaroteInferior
+from camarote1 import CamaroteSuperior
+
+ings1 = Normal()
+ings1.imprime_valor()
+ings1.imprimir()
+
+print('----------')
+
+ings2 = CamaroteInferior()
+ings2.imprime_valor()
+ings2.imprimir_localizacao()
+
+print('----------')
+
+ings3 = CamaroteSuperior()
+ings3.imprime_valor()
