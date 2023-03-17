96b474c04b7a4240e39864660cef59a4fc5b7e3c
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-17 14:09:32 -0300

    criação de testes no main

--- ingresso/main.py
+++ ingresso/main.py
@@ -2,17 +2,18 @@ from normal import Normal
 from camarote import CamaroteInferior
 from camarote1 import CamaroteSuperior
 
-ings1 = Normal()
-ings1.imprime_valor()
-ings1.imprimir()
+tipo_ingresso = int(input('Digite 1 para ingresso normal ou 2 para ingresso VIP: '))
+if tipo_ingresso == 1:
+    ings = Normal()
+    ings.imprime_valor()
+    ings.imprimir()
 
-print('----------')
-
-ings2 = CamaroteInferior()
-ings2.imprime_valor()
-ings2.imprimir_localizacao()
-
-print('----------')
-
-ings3 = CamaroteSuperior()
-ings3.imprime_valor()
+elif tipo_ingresso == 2:
+    tipo_vip = int(input('Digite 1 para camarote superior ou 2 para camarote inferior: '))
+    if tipo_vip == 1:
+        ings = CamaroteSuperior()
+        ings.imprime_valor()
+    elif tipo_vip == 2:
+        ings = CamaroteInferior()
+        ings.imprime_valor()
+        ings.imprimir_localizacao()
