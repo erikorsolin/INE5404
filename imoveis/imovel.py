a197231a82090380284d6ba366c8f2fa3b2e8bbc
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-17 13:49:25 -0300

    criação da classe  imovel

---
+++ imoveis/imovel.py
@@ -0,0 +1,13 @@
+class Imovel:
+    def __init__(self) -> None:
+        self.__endereco = 'Saco dos Limões, Rua João Motta Espezim'
+        self.__preco = 50000.00
+
+    def get_preco(self):
+        return self.__preco
+
+    def get_endereco(self):
+        return self.__endereco 
+    
+       
+        
