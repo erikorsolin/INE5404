813cdb99a165b237dfa5b4d760071f3e7e042fe9
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-17 13:55:03 -0300

    criação da classe novo

---
+++ imoveis/novo.py
@@ -0,0 +1,14 @@
+from imovel import Imovel
+
+class Novo(Imovel):
+    def __init__(self) -> None:
+        super().__init__()
+        p = self.get_preco() 
+        self.__novo_preco = p + 15/100*p
+
+    def get_novo_preco(self):
+        return self.__novo_preco
+    
+    def imprimir_novo_preco(self):
+        print(self.get_novo_preco())
+    
