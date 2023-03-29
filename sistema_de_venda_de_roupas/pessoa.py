af9ee2634475a113147ccfb02a3f6bfb6d7ba42c
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-28 23:21:16 -0300

    criação de funções gets e sets da classe Pessoa

--- sistema_de_venda_de_roupas/pessoa.py
+++ sistema_de_venda_de_roupas/pessoa.py
@@ -1,3 +1,17 @@
 class Pessoa:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, nome: str, cpf: str) -> None:
+        self.nome = nome
+        self.cpf = cpf 
+
+    def get_nome(self):
+        return self.nome
+    
+    def set_nome(self, nome):
+        self.nome = nome
+    
+    def get_cpf(self):
+        return self.cpf
+
+    def set_cpf(self, cpf):
+        self.cpf = cpf
+        
