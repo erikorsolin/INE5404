cba2cb5587b630f8c91d8ad1c8d3800c9e014c22
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 14:26:37 -0300

    definição dos métodos e dos atributos da classe cliente

--- sistema_de_venda_de_roupas/cliente.py
+++ sistema_de_venda_de_roupas/cliente.py
@@ -1,3 +1,10 @@
-class Cliente:
-    def __init__(self) -> None:
-        pass
+from pessoa import Pessoa
+
+class Cliente(Pessoa):
+    def __init__(self, nome: str, cpf: str, senha: str ):
+        super.__init__(nome, cpf)
+        self.senha = senha
+    
+    def get_senha(self):
+        return self.senha
+    
