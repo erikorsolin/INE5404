8fd648a8168a06d1afd0db1e7d711115f47781a0
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 18:00:25 -0300

    correção classe carrinho e mudança nos métodos

--- sistema_de_venda_de_roupas/carrinho.py
+++ sistema_de_venda_de_roupas/carrinho.py
@@ -18,15 +18,15 @@ class Carrinho:
         self.produtos.append(produto)
         self.preco_total += produto.get_preco()
         self.quantidade_produtos += 1
-        print(f"produto {produto.get_nome()} adicionado com sucesso")
+        print(f"produto  ({produto.get_nome()} | {produto.get_cor()} | {produto.get_tamanho()})  adicionado com sucesso")
     
-    def remover_produto(self, nome):
+    def remover_produto(self, id):
         for produto in self.produtos:
-            if produto.get_nome() == nome:
+            if produto.get_id() == id:
                 self.produtos.remove(produto)
                 self.quantidade_produtos -= 1
                 self.preco_total -= produto.get_preco()
-                print(f"produto {produto.get_nome()} removido com sucesso")
+                print(f"produto  ({produto.get_nome()} | {produto.get_cor()} | {produto.get_tamanho()})  removido com sucesso")
                 return
         print('Produto não está no carrinho')
 
