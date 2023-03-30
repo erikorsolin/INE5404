299b14ddf024fe4c87dcab6d165e3db96dcf01a8
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 16:05:01 -0300

    criação do main: controle de login e menu de ações

---
+++ sistema_de_venda_de_roupas/main.py
@@ -0,0 +1,44 @@
+from sistema_vendas import SistemaVendas
+from produto import Produto
+from cliente import Cliente
+
+loja = SistemaVendas()
+
+
+logado = False
+while True:
+    while logado == False:
+        decisao = int(input('Digite 1 se já possui possui uma conta ou 2 se deseja fazer o cadastro: '))
+        if decisao == 1:
+            cpf = input('Digite seu cpf: ')
+            senha = input('Digite sua senha: ')
+            cliente = loja.autenticar_cliente(cpf, senha)
+            if cliente == None:
+                print('Cliente não encontrado')
+            else:
+                print(f'É bom te ver novamente {cliente.get_nome()} :), você está logado!')
+                logado = True
+
+        elif decisao == 2:
+            nome = input('Digite seu nome: ')
+            cpf = input('Digite seu cpf: ')
+            senha = input('Digite sua senha: ')
+            cliente = Cliente(nome, cpf, senha)
+            loja.adicionar_cliente(cliente)
+            print(f'Bem vindo(a) {cliente.get_nome()}, você está logado!')
+            logado = True
+
+    print("="*20)
+    print('Opções do cliente')
+    print('1 - Ver produtos')
+    print('2 - Ver produtos disponíveis')
+    print('3 - Adicionar produto ao carrinho')
+    print('4 - Remover prproduto do carrinho')
+    print('5 - Consultar preço de um produto')
+    print('6 - Ver carrinho')
+    print('7 - Consultar preço total do carrinho')
+    print('8 - Finalizar compra')
+    print('9 - Deslogar')
+    print('10 - fechar programa')
+    acao = int(input('Digite a ação a ser tomada; '))
+
