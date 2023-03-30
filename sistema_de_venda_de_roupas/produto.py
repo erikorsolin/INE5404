2b065bb7373ae071bf9b39a7418ae89a21aa5ac3
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 14:15:43 -0300

    definição dos atributos e dos métodos da classe Produto

--- sistema_de_venda_de_roupas/produto.py
+++ sistema_de_venda_de_roupas/produto.py
@@ -1,3 +1,10 @@
 class Produto:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, nome, preco) -> None:
+        self.nome = nome
+        self.preco = preco
+    
+    def get_nome(self):
+        return self.nome
+    
+    def get_preco(self):
+        return self.preco
