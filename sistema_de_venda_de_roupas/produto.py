5c7a2c82a97781245ff80242b4886b3c7978f074
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 18:03:09 -0300

    mudança na classe Produto: adionado atributo ID

--- sistema_de_venda_de_roupas/produto.py
+++ sistema_de_venda_de_roupas/produto.py
@@ -1,10 +1,14 @@
 class Produto:
-    def __init__(self, nome: str, cor: str, tamanho: str, preco: float) -> None:
+    def __init__(self, id: int, nome: str, cor: str, tamanho: str, preco: float) -> None:
+        self.id = id
         self.nome = nome
         self.cor = cor
         self.tamanho = tamanho
         self.preco = preco
     
+    def get_id(self):
+        return self.id
+       
     def get_nome(self):
         return self.nome
     
