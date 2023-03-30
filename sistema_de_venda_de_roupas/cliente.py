fed30032118b476ac929f19ef58a606771b3ebd6
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 16:06:29 -0300

    correção na classe cliente: método super() corrigido

--- sistema_de_venda_de_roupas/cliente.py
+++ sistema_de_venda_de_roupas/cliente.py
@@ -2,7 +2,7 @@ from pessoa import Pessoa
 
 class Cliente(Pessoa):
     def __init__(self, nome: str, cpf: str, senha: str ):
-        super.__init__(nome, cpf)
+        super().__init__(nome, cpf)
         self.senha = senha
     
     def get_senha(self):
