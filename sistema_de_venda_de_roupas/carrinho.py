7563113394209ca83ecf0bdddd49a86ff680f998
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 14:10:52 -0300

    criação dos métodos e dos atributos da classe carrinho

--- sistema_de_venda_de_roupas/carrinho.py
+++ sistema_de_venda_de_roupas/carrinho.py
@@ -1,3 +1,33 @@
 class Carrinho:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, produtos, cliente) -> None:
+        self.produtos = list()
+        self.preco_total = 0
+        self.quantidade_produtos = 0
+
+    def get_produtos(self):
+        return self.produtos
+    
+    def get_preco_total(self):
+        return self.preco_total
+    
+    def get_quantidade_produtos(self):
+        return self.quantidade_produtos
+    
+    def adicionar_produto(self, produto):
+        self.produtos.append(produto)
+        self.preco_total += produto.get_preco()
+        self.quantidade_produtos += 1
+        print(f"produto {produto.get_nome()} adicionado com sucesso")
+    
+    def remover_produto(self, nome):
+        for produto in self.produtos:
+            if produto.get_nome() == nome:
+                self.produtos.remove(produto)
+                self.quantidade_produtos -= 1
+                self.preco_total -= produto.get_preco()
+                print(f"produto {produto.get_nome()} removido com sucesso")
+                return
+        print('Produto não está no carrinho')
+
+
+   
