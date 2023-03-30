25e5ab77a04e42560c789ed7c4cff515d1b4885a
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 16:03:44 -0300

    criação da classe sistema_vendas e definição de atributos e métodos

---
+++ sistema_de_venda_de_roupas/sistema_vendas.py
@@ -0,0 +1,34 @@
+from carrinho import Carrinho
+class SistemaVendas:
+    def __init__(self):
+        self.produtos = []
+        self.clientes = []
+        self.vendas = []
+
+    def adicionar_produto(self, produto):
+        self.produtos.append(produto)
+
+    def remover_produto(self, produto):
+        self.produtos.remove(produto)
+
+    def buscar_produto_por_nome(self, nome):
+        for produto in self.produtos:
+            if produto.nome == nome:
+                return produto
+        return None
+
+    def adicionar_cliente(self, cliente):
+        self.clientes.append(cliente)
+
+    def remover_cliente(self, cliente):
+        self.clientes.remove(cliente)
+
+    def autenticar_cliente(self, cpf, senha):
+        for cliente in self.clientes:
+            if cliente.cpf == cpf and cliente.senha == senha:
+                return cliente
+        return None
+
+    def registrar_venda(self, cliente):
+        venda = Carrinho(cliente)
+        self.vendas.append(venda)
