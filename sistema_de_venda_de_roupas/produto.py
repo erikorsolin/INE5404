b3718e117511560b6dd04fd6aa2021e99da9dc75
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-30 17:30:07 -0300

    adicionando novos atributos e métodos na classe produto

--- sistema_de_venda_de_roupas/produto.py
+++ sistema_de_venda_de_roupas/produto.py
@@ -1,10 +1,18 @@
 class Produto:
-    def __init__(self, nome, preco) -> None:
+    def __init__(self, nome: str, cor: str, tamanho: str, preco: float) -> None:
         self.nome = nome
+        self.cor = cor
+        self.tamanho = tamanho
         self.preco = preco
     
     def get_nome(self):
         return self.nome
+    
+    def get_cor(self):
+        return self.cor
+    
+    def get_tamanho(self):
+        return self.tamanho
     
     def get_preco(self):
         return self.preco
