32aaf62057f71cb314f8d1bdfa3605e9da78b7c4
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-31 14:07:23 -0300

    correção na classe cliente: importando carrinho e instanciando carrinho dentro de cliente

--- sistema_de_venda_de_roupas/cliente.py
+++ sistema_de_venda_de_roupas/cliente.py
@@ -1,10 +1,11 @@
 from pessoa import Pessoa
+from carrinho import Carrinho
 
 class Cliente(Pessoa):
-    def __init__(self, nome: str, cpf: str, senha: str, carrinho: object ):
+    def __init__(self, nome: str, cpf: str, senha: str):
         super().__init__(nome, cpf)
         self.senha = senha
-        self.carrinho = carrinho
+        self.carrinho = Carrinho()
     
     def get_senha(self):
         return self.senha
