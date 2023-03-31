86f0390a79c1c4146d3d848a777cf18b02752404
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-31 13:47:57 -0300

    colocando o objeto carrinho como atributo da classe cliente

--- sistema_de_venda_de_roupas/cliente.py
+++ sistema_de_venda_de_roupas/cliente.py
@@ -1,9 +1,10 @@
 from pessoa import Pessoa
 
 class Cliente(Pessoa):
-    def __init__(self, nome: str, cpf: str, senha: str ):
+    def __init__(self, nome: str, cpf: str, senha: str, carrinho: object ):
         super().__init__(nome, cpf)
         self.senha = senha
+        self.carrinho = carrinho
     
     def get_senha(self):
         return self.senha
